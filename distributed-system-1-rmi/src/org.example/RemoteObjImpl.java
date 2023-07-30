32b2166d56191fee4e5cee06355cc18428d7294d
xineonrie <xineonrie@gmail.com>
2023-07-31 03:01:26 +0930

    feat: init with error :(

---
+++ distributed-system-1-rmi/src/org.example/RemoteObjImpl.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RemoteObjImpl extends UnicastRemoteObject implements IRemoteObj {
+    
+}
